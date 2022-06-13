4d2f9bdb2509c92c9c2a3adfe359832e641907ac
Robar3 <terehov94@mail.ru>
2022-06-13 18:01:53 +0300

    lesson 6 at webinar

--- src/main/java/lesson6/EchoServer.java
+++ src/main/java/lesson6/EchoServer.java
@@ -5,24 +5,46 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoServer {
     public static void main(String[] args) {
-        Socket socket =null;
-        try(ServerSocket serverSocket = new ServerSocket(8189);) {
+        Socket socket;
+        try (ServerSocket serverSocket = new ServerSocket(8189);) {
             System.out.println("Сервер запущен");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
             final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-            while (true){
-                final String message =inputStream.readUTF();
-                System.out.println("Сообщение от клиента: "+message);
-                if ("/end".equalsIgnoreCase(message)){
+            Thread thread = new Thread(() -> {
+                try {
+                    final Scanner scanner = new Scanner(System.in);
+                    while (true) {
+                        final String msg = scanner.nextLine();
+                        if (msg.isEmpty()){
+
+                        }else {
+                            outputStream.writeUTF(msg);
+                            if ("/end".equalsIgnoreCase(msg)) {
+                                break;
+                            }
+                        }
+
+
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            thread.start();
+            while (true) {
+                final String message = inputStream.readUTF();
+                System.out.println("Сообщение от клиента: " + message);
+                if ("/end".equalsIgnoreCase(message)) {
                     outputStream.writeUTF("/end");
                     break;
                 }
-                outputStream.writeUTF("Echo: "+message);
+                outputStream.writeUTF("Echo: " + message);
 
             }
         } catch (IOException e) {
